06e4db1940ab5986c8d2f25680158745f48dfb59
pacelli3 <flores.eugenio03@gmail.com>
2026-06-14 12:02:56 -0500

    chore(browser-extension-manager-ui): installed @netlify/vite-plugin-react-router as a devDependency

--- browser-extension-manager-ui/vite.config.ts
+++ browser-extension-manager-ui/vite.config.ts
@@ -1,8 +1,8 @@
 import {defineConfig} from "vite";
 import {reactRouter} from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 import netlify from "@netlify/vite-plugin";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
     plugins: [tailwindcss(), reactRouter(), netlifyReactRouter(), netlify()],
